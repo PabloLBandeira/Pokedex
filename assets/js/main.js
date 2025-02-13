0272ae05aff80444d5eb63e4ff09c96f82ec9a81
PabloBandeira <pablo.bandeira@ymail.com>
2025-02-13 14:38:40 -0300

    wip: Create modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,91 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modalTitle");
+const pokemonImage = document.getElementById("pokemonImage");
+const fade = document.getElementById("fade");
 
-function convertPokemonTypesToLi (pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+const limit = 12;
+let offset = 0;
+const maxRecord = 151;
+
+function loadPokemons(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}" data-number="${pokemon.number}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>                        
+                    <img src="${pokemon.image}" alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('');
+
+    pokemonList.innerHTML += newHtml;
+
+    document.querySelectorAll(".pokemon").forEach((pokemonElement) => {
+        pokemonElement.addEventListener("click", () => {
+            const pokemonNumber = pokemonElement.getAttribute("data-number");
+            const selectedPokemon = pokemons.find(p => p.number == pokemonNumber);
+            
+            if (selectedPokemon) {
+                openModal(selectedPokemon);
+            }
+            });
+        });
+    });
 }
 
-function convertPokemonHTML(pokemon) {
-    return`
-    <li class="pokemon">
-                    <span class="number" >#${pokemon.order}</span>
-                    <span class="name" >${pokemon.name}</span>
-                    
-                    <div class="detail" >
-                        <ol class="types">
-                            ${convertPokemonTypesToLi (pokemon.types).join('')}
-                        </ol>                        
-                        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-                    </div>
+loadPokemons(offset, limit);
 
-                </li>
-    `
-}
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdNextPage = offset + limit;
+    if (qtdNextPage >= maxRecord) {
+        const newLimit = maxRecord - offset;
+        loadPokemons(offset, newLimit);
 
-const pokemonList = document.getElementById('pokemonList')
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemons(offset, limit);
+    }
+});
+
+const openModal = (pokemon) => {
+    modalTitle.textContent = pokemon.name;
+    pokemonImage.src = pokemon.image;
+    pokemonImage.alt = pokemon.name;
 
 
-pokeApi.getPokemons().then((pokemons = []) => {
+    
 
-    const newHtml = pokemons.map(convertPokemonHTML).join('')
-    pokemonList.innerHTML = newHtml
-})
+    pokeApi.getPokemonData(pokemon.name).then((pokemonData) => {
+        const pokemonDetails = `
+            <ol>
+            <li><strong>Height</strong>: ${pokemonData.height}</li>
+            <li><strong>Weight</strong>: ${pokemonData.weight}</li>
+            <li><strong>Abilities</strong>: ${pokemonData.abilities.join(', ')}</li>
+            </ol>
+            <ol>
+            <li><strong>Base Stats</strong>:</li>
+            ${pokemonData.baseStats.map(stat => `<li>${stat.stat}: ${stat.value}</li>`).join('')}
+            </ol>
+        `;
+        document.getElementById("pokemonDetails").innerHTML = pokemonDetails;
+    });
+
+    modal.classList.remove("hide");
+    fade.classList.remove("hide");
+};
+
+const closeModal = () => {
+    modal.classList.add("hide");
+    fade.classList.add("hide");
+};
+
+document.querySelector(".close").addEventListener("click", closeModal);
+fade.addEventListener("click", closeModal);
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,29 @@
 
 const pokeApi = {}
 
+function convertPokeApiToPokemon (pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.image = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}    
+
 pokeApi.getPokemonsDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiToPokemon)
 }
 
-pokeApi.getPokemons = (offset=0, limit=10) =>{
+pokeApi.getPokemons = (offset, limit) =>{
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
@@ -16,6 +34,24 @@ pokeApi.getPokemons = (offset=0, limit=10) =>{
         .then((pokemonsDetails) => pokemonsDetails)
 
         .catch((error) => console.error(error))
-
 }
 
+pokeApi.getPokemonData = (pokemonName) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((pokeDetail) => {
+            const pokemonData = {
+                height: pokeDetail.height,
+                weight: pokeDetail.weight,
+                abilities: pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name),
+                baseStats: pokeDetail.stats.map((statSlot) => ({
+                    stat: statSlot.stat.name,
+                    value: statSlot.base_stat
+                }))
+            }
+            return pokemonData
+        })
+        .catch((error) => console.error(error))
+}
